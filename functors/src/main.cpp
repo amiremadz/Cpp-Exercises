2a3bdff74f5a5bb12108257400cdd7aae4ab82df
Amir <aemadzadeh@nvidia.com>
2017-09-05 14:18:59 -0700

    Practice functors.

---
+++ functors/src/main.cpp
@@ -0,0 +1,52 @@
+//============================================================================
+// Name        : functors.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+struct Test{
+	virtual bool operator()(string &txt) = 0;
+	virtual ~Test() {}
+};
+
+
+// Functor: it overloads paranthesis operator
+struct MatchTest : public Test{
+	virtual bool operator()(string &txt){
+		return txt == "lion";
+	}
+	virtual ~MatchTest() {}
+};
+
+void check(string text, Test &test){
+	if(test(text)){
+		cout << "Strings match!" << endl;
+	}
+	else{
+		cout << "Strings do not macth!" << endl;
+	}
+}
+
+int main() {
+
+	MatchTest pred;
+	string value = "liond";
+
+	// cannot use literal value here
+	// pred("lion")
+
+	cout << pred(value) << endl;
+
+	cout << endl;
+
+	check("lion", pred);
+	check("cat", pred);
+
+
+	return 0;
+}
