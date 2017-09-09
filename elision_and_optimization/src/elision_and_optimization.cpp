71c9e79f5dfd431e72947fd94a6ec8067231c588
Amir <aemadzadeh@nvidia.com>
2017-09-09 11:53:04 -0700

    Constructors and memory.

--- elision_and_optimization/src/elision_and_optimization.cpp
+++ elision_and_optimization/src/elision_and_optimization.cpp
@@ -7,30 +7,44 @@
 //============================================================================
 
 #include <iostream>
+#include <memory.h>
+
 using namespace std;
 
 class Test{
+private:
+	static const int kSIZE = 100;
+	int *m_pBuffer;
 public:
 	Test(){
 		cout << "empty constructor" << endl;
+		m_pBuffer = new int[kSIZE]{};  				// or {}
+		//memset(m_pBuffer, 0, kSIZE*sizeof(int));  // from memory library
+
 	}
-	Test(int i){
+	Test(int i) : Test(){
 		cout << "parameterized constructor" << endl;
+		for(int i=0; i<kSIZE; i++){
+			m_pBuffer[i] = 7*i;
+		}
 	}
 	Test(const Test &other){
 		cout << "copy constructor" << endl;
-		*this = other;
+		m_pBuffer = new int[kSIZE]{};
+		memcpy(m_pBuffer, other.m_pBuffer, kSIZE*sizeof(int));
 	}
 public:
 	Test operator=(const Test &other){
 		cout << "assignment" << endl;
-		*this = other;
+		m_pBuffer = new int[kSIZE]{};
+		memcpy(m_pBuffer, other.m_pBuffer, kSIZE*sizeof(int));
 		return *this;
 	}
 	friend ostream &operator<<(ostream &out, const Test &test);
 public:
 	~Test(){
 		cout << "destructor" << endl;
+		delete[] m_pBuffer;
 	}
 };
 
@@ -46,7 +60,7 @@ Test getTest(){
 //‑fno‑elide‑constructors
 int main() {
 
-	// without optimization:
+	// Without optimization:
 	// constructor: Test()
 	// copy constructor: return->copy to temporary return value
 	// destructur: of Test()
