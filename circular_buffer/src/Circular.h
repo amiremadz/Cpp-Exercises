18b48cfd566ea7c397f379363cabf722ce2e7c56
Amir <aemadzadeh@nvidia.com>
2017-09-11 16:18:06 -0700

    Create a circular buffer class.

---
+++ circular_buffer/src/Circular.h
@@ -0,0 +1,147 @@
+/*
+ * Circular.h
+ *
+ *  Created on: Sep 11, 2017
+ *      Author: aemadzadeh
+ */
+
+#ifndef CIRCULAR_H_
+#define CIRCULAR_H_
+
+#include <iostream>
+#include <initializer_list>
+#include <cstring>
+
+using namespace std;
+
+template <typename T>
+class Circular {
+private:
+	size_t mSize{0};
+	T* mBuffer{nullptr};
+	size_t mIndex{0};
+
+public:
+	Circular() = default;
+
+	Circular(size_t size): mSize(size){
+		mBuffer = new T[size];
+	}
+
+	Circular(initializer_list<T> list){
+		mSize = list.size();
+		mBuffer = new T[mSize];
+		add(list);
+	}
+
+public:
+	Circular(const Circular<T> &other){
+		//cout << "copy" << endl;
+		mSize = other.mSize;
+		mIndex = other.mIndex;
+		mBuffer = new T[mSize]{};
+		memcpy(mBuffer, other.mBuffer, mSize*sizeof(T));
+	}
+
+	Circular(Circular<T> &&other){
+		//cout << "move" << endl;
+		mSize = other.mSize;
+		mIndex = other.mIndex;
+		mBuffer = other.mBuffer;
+		other.mBuffer = nullptr;
+	}
+
+public:
+	template<typename S>
+	friend ostream &operator<<(ostream &out, const Circular<S> &buffer);
+
+	T operator[](size_t index) const {
+		return mBuffer[index];
+	}
+
+	Circular operator=(const Circular &other){
+		mSize = other.mSize;
+		mIndex = other.mIndex;
+		mBuffer = new T[mSize];
+		memcpy(mBuffer, other.mBuffer, mSize*sizeof(T));
+		return *this;
+	}
+
+public:
+	void add(T value) {
+		mBuffer[mIndex] = value;
+		mIndex++;
+		if(mIndex >= mSize){
+			mIndex = 0;
+		}
+	}
+
+	void add(initializer_list<T> list){
+		for(auto item : list){
+			add(item);
+		}
+	}
+
+	size_t size() const { return mSize; }
+
+public:
+	class iterator;
+
+public:
+	iterator begin() {
+		return iterator(0, *this);
+	}
+
+	iterator end(){
+		return iterator(mSize, *this);
+	}
+
+public:
+	~Circular(){
+		delete[] mBuffer;
+	}
+};
+
+template<typename S>
+ostream &operator<<(ostream &out, const Circular<S> &circ_buffer){
+	for(size_t i=0; i<circ_buffer.mSize; i++){
+		out << circ_buffer.mBuffer[i] << " ";
+	}
+	return out;
+}
+
+
+template<typename T>
+class Circular<T>::iterator{
+private:
+	size_t mIndex{0};
+	Circular<T> &mCircular;
+public:
+	iterator(size_t index, Circular<T> &circular): mIndex(index), mCircular(circular){
+		//mCircular = circular; // does not work: a reference cannot be reassigned.
+								// so it must be initialized at the member-initialization-list.
+	}
+
+public:
+	iterator operator++(){
+		mIndex++;
+		return *this;
+	}
+
+	iterator operator++(int){
+		mIndex++;
+		return *this;
+	}
+
+	T operator*() const {
+		return mCircular.mBuffer[mIndex];
+	}
+
+	bool operator!=(const iterator &other) const {
+		return mIndex != other.mIndex;
+	}
+};
+
+
+
+#endif /* CIRCULAR_H_ */
---
+++ circular_buffer/src/circular_buffer.cpp
@@ -0,0 +1,62 @@
+//============================================================================
+// Name        : circular_buffer.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Circular.h"
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+
+	Circular<int> buff{1, 2, 3, 4};
+
+	cout << buff << endl;
+
+	buff.add(5);
+
+	cout << buff << endl;
+
+	buff.add({6, 7});
+
+	cout << buff << endl;
+
+	Circular<int> cpy_buff(buff);
+
+	cout << cpy_buff << endl;
+
+	cout << cpy_buff[2] << endl;
+
+	Circular<int> assgn_buff;
+	assgn_buff = cpy_buff;
+
+	cout << assgn_buff	<< endl;
+
+	cout << assgn_buff[0]	<< endl;
+
+	vector< Circular<int> > vec{ Circular<int>{3, 4} };
+
+	// Move constructor
+	vec.push_back(Circular<int>{6, 7, 8});
+
+	cout << vec[0] << endl;
+
+	for(auto val : buff){
+		cout << val << endl;
+	}
+
+	cout << endl;
+
+	for(Circular<int>::iterator it=buff.begin(); it != buff.end(); it++){
+		cout << *it << endl;
+	}
+
+	return 0;
+}
