203f408ed8d687bcb4c8be9759b4b89a66a9feb7
Amir <aemadzadeh@nvidia.com>
2017-11-28 13:15:42 -0800

    Check if same class: partial specialization for functions does not work.

--- check_if_sameclass/main.cpp
+++ check_if_sameclass/main.cpp
@@ -19,13 +19,25 @@ bool isSameClass(){
     return isSame<A, B>::Is;
 }
 
+template<typename T, typename U>
+bool same(){
+    return false;
+}
 
+// does not work: partial specialization
+/*
+template<typename T>
+bool same<T, T>(){
+    return true;
+}
+*/
 
 int main(){
     
     cout << isSameClass<int, int>() << endl;
     cout << isSameClass<int, float>() << endl;
-
+    cout << same<int, float>() << endl;
+    cout << same<float, float>() << endl;
 
     return 0;
 }
