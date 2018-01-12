1f0264ccda0b98c000ca3d6d0786cfb825e1e4a0
Amir <aemadzadeh@nvidia.com>
2018-01-12 12:05:51 -0800

    Subsets: iterative solution.

---
+++ leetcode/78_subsets/main.cpp
@@ -0,0 +1,98 @@
+/*
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Iterative {
+public:
+    vector<vector<int>> subsets_1(vector<int>& nums) {
+        vector<vector<int>> subs(1, vector<int>());
+        sort(nums.begin(), nums.end());
+        
+        for(int num : nums){
+            int n = subs.size();
+            for(int idx = 0; idx < n; ++idx){
+                vector<int> new_sub = subs[idx];
+                new_sub.push_back(num);
+                subs.push_back(new_sub);        
+            }
+        }
+              
+        return subs;
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs(1, vector<int>());
+        sort(nums.begin(), nums.end());
+        
+        for(int num : nums){
+            int n = subs.size();
+            for(int idx = 0; idx < n; ++idx){
+                subs.push_back(subs[idx]);
+                subs.back().push_back(num);
+            }
+        }
+              
+        return subs;
+    }
+
+
+    void print(vector<int> vec){
+    	for(int x : vec){
+    		cout << x << " " << flush;
+    	}
+    	cout << endl;
+    }
+
+    void print(vector<vector<int>> sets){
+    	for(auto set : sets){
+    		if(set.empty()){
+    			cout << "[]" << endl;
+    			continue;
+    		}
+
+    		for(int num : set){
+    			cout << num << " " << flush;
+    		}
+    		cout << endl;
+    	}
+    	cout << endl;
+    }
+};
+
+
+int main(){
+
+	vector<int> vec = {1, 2, 3};
+	vector<vector<int>> subsets;
+	Iterative sol_itt; 
+
+	subsets = sol_itt.subsets(vec);
+	
+	sol_itt.print(vec);
+	cout << endl;
+    sol_itt.print(subsets);
+
+	return 0;
+}
