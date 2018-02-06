00e407bf95a311d5dff5d7ed7f5d8663a115b17d
Amir Emadzadeh <amire@ucla.edu>
2018-02-06 12:23:47 -0800

    Sorted matrix: count negative ints.

--- leetcode/count_neg_matrix/main.cpp
+++ leetcode/count_neg_matrix/main.cpp
@@ -7,20 +7,72 @@ negative integeres.
 
 using namespace std;
 
-
 int count(vector<vector<int>> mat){
-	int cnt = 0;
+	int count = 0;
+    int num_rows = mat.size();
+    int num_cols = mat[0].size();
+    
+    int row = 0;
+    int col = num_cols - 1;
+    
+    while(row < num_rows){
+        while(col >= 0 && mat[row][col] > 0){
+            --col;
+        }
+        if(col >= 0){
+            count += (col + 1);
+        }
+        else{
+            break;
+        }
+        ++row;
+    }
 
-	return cnt;
+	return count;
 }
 
+int count(vector<vector<int>> mat){
+    int count = 0;
+    int num_rows = mat.size();
+    int num_cols = mat[0].size();
+    
+    int row = 0;
+    int col = num_cols - 1;
+    
+    while(row < num_rows && col >= 0){
+        if(mat[row][col] < 0){
+            count += (col + 1);
+            ++row;
+        }
+        else{
+            --col;
+        }
+    }
+
+    return count;
+}
 
 int main(){
-	vector<vector<int>> mat = { {-3, -2, -1, 1},
-								{-2, 2, 3, 4},
-								{4, 5, ,7, 8} };
+	vector<vector<int>> mat1 = { {-3, -2, -1, 1},
+								 {-2, 2, 3, 4},
+								 {4, 5, 7, 8} };
 
-	cout << count(mat) << endl;
+	vector<vector<int>> mat2 = { {3, 4, 5, 6},
+								 {4, 5, 6, 7},
+								 {5, 6, 7, 8} };
+
+    vector<vector<int>> mat3 = { {-3, 2, 1, 1},
+								 {1, 2, 3, 4},
+								 {4, 5, 7, 8} };
+
+    vector<vector<int>> mat4 = { {-4, -3, -2, -1},
+                                 {1, 2, 3, 4},
+                                 {4, 5, 7, 8} };
+
+    cout << count(mat1) << endl;
+    cout << count(mat2) << endl;
+    cout << count(mat3) << endl;
+    cout << count(mat4) << endl;
 
 	return 0;
 }
