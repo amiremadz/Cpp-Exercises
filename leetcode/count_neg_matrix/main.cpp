dc74922200b88e2d648a65fb9e0bf5668879ae4b
amiremadz <amire@ucla.edu>
2018-02-06 08:47:42 -0800

    Sorted matrix: count negatives (body).

---
+++ leetcode/count_neg_matrix/main.cpp
@@ -0,0 +1,26 @@
+/*
+Given a ro/comun-wise soreted matrix, count the number of
+negative integeres.
+*/
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int count(vector<vector<int>> mat){
+	int cnt = 0;
+
+	return cnt;
+}
+
+
+int main(){
+	vector<vector<int>> mat = { {-3, -2, -1, 1},
+								{-2, 2, 3, 4},
+								{4, 5, ,7, 8} };
+
+	cout << count(mat) << endl;
+
+	return 0;
+}
