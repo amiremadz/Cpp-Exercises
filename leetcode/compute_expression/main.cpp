e59eea3dbbd19c7e24ee2ff2228fdefd91cc87a6
amiremadz <amire@ucla.edu>
2018-02-27 09:17:47 -0800

    Compute expression: fixed bug: * at the end.

--- leetcode/compute_expression/main.cpp
+++ leetcode/compute_expression/main.cpp
@@ -39,7 +39,15 @@ int compute(string str){
             }
         }
     }
+
     numbers.push_back(atoi(num.c_str()));
+    ++size;
+
+    if(multiply){
+        numbers[size - 2] *= numbers[size - 1];
+        numbers.pop_back();
+        --size;
+    }
 
     for(int item:numbers){
         result += item;
@@ -51,8 +59,10 @@ int compute(string str){
 int main(){
     string str1 = "120+12*10*2+33";
     string str2 = "123+45*10+66";
+    string str3 = "120+5*10*2";
     cout << compute(str1) << "-" << 120 + 12*10*2 + 33 << endl;
     cout << compute(str2) << "-" << 123 + 45*10 + 66 << endl;
+    cout << compute(str3) << "-" << 120 + 5*10*2 << endl;
 
     return 0;
 }
